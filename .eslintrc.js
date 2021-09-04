35401a4dcd8077943f07701207286a8f1f06d681
Robert <robert.yang@onebyus.com>
2021-09-04 22:48:12 +0400

    eslint 自定义规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,18 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020
   },
+  // 自定义编校验规则
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: true
+        }
+      }
+    ]
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <div id="app">
     <router-view />
+    <div></div>
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
 
-<style lang="scss" scoped>
-</style>
+interface Foo {
+  a: string
+  b: string
+}
+
+export default Vue.extend({
+  name: 'App'
+})
+</script>
+
+<style lang="scss" scoped></style>
