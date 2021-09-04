fb87d707e8e200573434dba09e3436999763df79
Robert <robert.yang@onebyus.com>
2021-09-04 22:57:05 +0400

    ElementUI 引入

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <template>
   <div id="app">
     <router-view />
-    <div></div>
   </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
-
-interface Foo {
-  a: string
-  b: string
-}
 
 export default Vue.extend({
   name: 'App'
